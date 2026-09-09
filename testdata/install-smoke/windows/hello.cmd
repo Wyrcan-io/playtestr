9cf00e89f378703d66b6bc05978777c6dc8e2ec1
abhirupone <abhirup.karmakar00@gmail.com>
2026-09-09 14:34:45 +0530

    ci: use native Windows install fixture

---
+++ testdata/install-smoke/windows/hello.cmd
@@ -0,0 +1,5 @@
+@echo off
+set /p name=Name:
+echo Hello, %name%!
+set /p ignored=Press Enter to exit:
+exit /b 0
--- testdata/install-smoke/windows/hello.ps1
+++
@@ -1,3 +0,0 @@
-$name = Read-Host 'Name'
-Write-Host "Hello, $name!"
-Read-Host 'Press Enter to exit' | Out-Null
